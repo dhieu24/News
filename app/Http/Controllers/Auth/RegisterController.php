8ba655e964f4465f54f63f5e3c16362a90187e1b
hieudangk27 <hieudangk27@gmail.com>
2023-05-02 18:37:33 -0400

    change method in registercontroller to index

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -47,7 +47,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
+    public function index(array $data)
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
