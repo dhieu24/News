09680b0886aa4d33ee83c23a54c9712baf9473ae
hieudangk27 <hieudangk27@gmail.com>
2023-05-02 17:44:59 -0400

    add vite command in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
     <body class="bg-gray-100">
         <nav class="p-6 bg-white flex justify-between mb-4">
