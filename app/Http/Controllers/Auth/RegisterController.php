0959ee39c6bcbcebe1c1f4aa2abfeed983787080
hieudangk27 <hieudangk27@gmail.com>
2023-05-02 23:12:51 -0400

    Handle case duplicate email, redirect back to register page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,22 +50,27 @@ class RegisterController extends Controller
     public function store(Request $request)
     {
 
-        $this->validate($request, [
-            'name' => 'required|max:255',
-            'username' => 'required|max:255',
-            'email' => 'required|email|max:255',
-            'password' => 'required|confirmed'
-        ]);
-
-        User::create([
-            'name' => $request->name,
-            'username' => $request->username,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]);
-
-        auth()->attempt($request->only('email', 'password'));
-
-        return redirect()->route('dashboard');
+        try{
+            $this->validate($request, [
+                'name' => 'required|max:255',
+                'username' => 'required|max:255',
+                'email' => 'required|email|max:255',
+                'password' => 'required|confirmed'
+            ]);
+    
+            User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+    
+            auth()->attempt($request->only('email', 'password'));
+    
+            return redirect()->route('dashboard');
+        }catch(\Illuminate\Database\QueryException $e){
+            return back()->with('email', 'Duplicate email!');
+        }
+        
     }
 }
